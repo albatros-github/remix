6a551cb5acf2b90796319c3a10d5ed5fc82075dd
albatros-github <106282806+albatros-github@users.noreply.github.com>
2022-06-02 07:26:02 +0000

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,18 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
-contract AllTest {
+// ----------- START Owned.sol file for import (add pragma ...) ------------
+//import "./Owned.sol";
+
+contract Owned {    
+    address public owner;
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "you are not the smarth contract owner");
+        //paste upper line with content of modified function and then replace all copied code inside function again
+        _;
+    }
+
+}
+// -----------  END Owned.sol file for import ------------
+
+contract AllTest is Owned {
     string public myString = "hi!";
     bool public paused;
     uint public myUint;
     uint public balanceReceive;
     uint public balanceReceive2;
     uint public lockedUntil;
-    address public owner;
+    //inherited from Owned contract
+    //address public owner;
     address public tmpaddr;
     mapping(uint => bool) public mappedUint;
     mapping(address => bool) public mappedAddress;
-    mapping(address => Balance) mappedAddressBalance;
+    mapping(address => uint8) tokens;
+    mapping(address => Balance) public mappedAddressBalance;
     mapping(uint  => mapping(uint => bool)) uintUintBoolMapping;
 
     struct Payment {
@@ -27,7 +47,9 @@ contract AllTest {
     }
 
     constructor() {
-        owner = msg.sender;
+        //inherited from Owned contract
+        //owner = payable(msg.sender);
+        tokens[owner] = 100;
     }
 
     function setUint(uint _myUint) public {
@@ -52,8 +74,9 @@ contract AllTest {
         myString = _string;
     }
 
-    function setPause(bool _pause) public {
-        require(owner == msg.sender, "you are not the smarth contract owner");
+    function setPause(bool _pause) public onlyOwner{
+        //inherited from Owned contract
+        //require(owner == msg.sender, "you are not the smarth contract owner");
         paused = _pause;
     }
 
@@ -63,6 +86,7 @@ contract AllTest {
     }
 
     function receiveMoneyTracked() public payable {
+        assert(mappedAddressBalance[msg.sender].totalBalance + msg.value >= mappedAddressBalance[msg.sender].totalBalance);
         mappedAddressBalance[msg.sender].totalBalance += msg.value;
         Payment memory payment = Payment(msg.value,block.timestamp);
         mappedAddressBalance[msg.sender].payment[mappedAddressBalance[msg.sender].numPayments] = payment;
@@ -72,9 +96,11 @@ contract AllTest {
 
     function withdrawMoneyOwnerOnly(uint _amount, address payable _to) public {
         require(_amount <= mappedAddressBalance[msg.sender].totalBalance,"Not enought liquidity");
-        require(owner == msg.sender, "you are not the smarth contract owner");
+        //inherited from Owned contract
+        //require(owner == msg.sender, "you are not the smarth contract owner");
         require(paused == false, "smarth contract paused");
-        
+        //assert doesn't return transfer fumnds, but require does
+        assert(mappedAddressBalance[msg.sender].totalBalance >= mappedAddressBalance[msg.sender].totalBalance - _amount);
         if(lockedUntil < block.timestamp){ 
             mappedAddressBalance[msg.sender].totalBalance -= _amount;
             _to.transfer(_amount);
@@ -82,7 +108,9 @@ contract AllTest {
     }
 
     function selfDestruct(address payable _address) public {
-        require(owner == msg.sender, "you are not the smarth contract owner");
+        //inherited from Owned contract
+        //require(owner == msg.sender, "you are not the smarth contract owner");
+
         //WILL BE DEPRECATED: change the state so the S.C. addrs will not have any code and you could not interact with the logic but possible to send to it losing your money
         //before being destroyed the _address will receive all the S.C. fonds
         selfdestruct(_address);
@@ -102,5 +130,38 @@ contract AllTest {
 
     function getUintUintBoolMapping(uint _index1, uint _index2) view public returns (bool) {
         return uintUintBoolMapping[_index1][_index2];
+    }
+
+    fallback() external payable {
+        getBalanceSmarthContract();        
+    }
+
+    receive() external payable {
+        receiveMoneyTracked();        
+    }
+
+    //pure funcions can not access STATE VARIABLES (kind of "class" variables)
+    function convertWeiToEther(uint _weiAmount) public pure returns(uint ) {
+        return _weiAmount / 1 ether;
+    }
+}
+
+
+// ERROR HANDLING
+contract ThrowError {
+    function errFunction() public pure {
+        require(false, "parse error");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    function catchError() public {
+        ThrowError err = new ThrowError();
+        try err.errFunction() {
+            //code if it works
+        }catch Error(string memory reason){
+            emit ErrorLogging(reason);
+        }
     }
 }
