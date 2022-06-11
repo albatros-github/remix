1aef9ece947a4304b7a47201085982a45ee0789d
albatros-github <106282806+albatros-github@users.noreply.github.com>
2022-06-11 05:42:10 +0000

    Update ItemManager.sol

--- SupplyChain/ItemManager.sol
+++ SupplyChain/ItemManager.sol
@@ -1,12 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract ItemManager{
+import "./Item.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-    enum SupplyChainSteps{Created, Paid,Delivered}
+contract ItemManager is Ownable {
+
+    enum SupplyChainSteps{NoStock, Created, Paid, Delivered}
 
     struct S_Item{
-        uint _priceInWei;
+        Item _item;
+        //uint _priceInWei;
         ItemManager.SupplyChainSteps _step;
         string _identifier;
     }
@@ -14,26 +18,32 @@ contract ItemManager{
     mapping(uint => S_Item) public items;
     uint index;
 
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address indexed _address);
 
-    function createItem(string memory _identifier, uint _priceInWei) public {
-        items[index]._priceInWei = _priceInWei;
+    function createItem(string memory _identifier, uint _priceInWei) public onlyOwner {
+        Item item = new Item(this, _priceInWei, index);
+        items[index]._item = item;
+        //items[index]._priceInWei = _priceInWei;
         items[index]._identifier = _identifier;
         items[index]._step = SupplyChainSteps.Created;
-        emit SupplyChainStep(index,uint(items[index]._step)); //casted enum (values from 0, 1,..) to uint 
+        emit SupplyChainStep(index,uint(items[index]._step), address(item)); //casted enum (values from 0, 1,..) to uint 
         index++;
     }
 
-    function triggerPyment(uint _index) public payable {
-        require(items[index]._priceInWei <= msg.value,"Not fully paid");
-        require(items[index]._step == SupplyChainSteps.Created, "item advanced in supplty chain");
-        items[_index]._step == SupplyChainSteps.Paid;
-        emit SupplyChainStep(_index,uint(items[_index]._step)); 
+    function triggerPayment(uint _index) public payable {
+        Item item = items[_index]._item;
+        //PENDING TO CHECK INVOCATION
+        //require(address(item) == msg.sender, "Only items are allowed to update themselves");
+        require(item.priceInWei() == msg.value,"Not fully paid");
+        require(items[_index]._step == SupplyChainSteps.Created, "item advanced in supplty chain");
+        items[_index]._step = SupplyChainSteps.Paid;
+        emit SupplyChainStep(_index,uint(items[_index]._step), address(item)); 
     }
 
-    function triggerDelivery(uint _index) public {
-        require(items[index]._step == SupplyChainSteps.Paid, "item not paid");
-        items[_index]._step == SupplyChainSteps.Delivered;
-        emit SupplyChainStep(_index,uint(items[_index]._step)); 
+    function triggerDelivery(uint _index) public onlyOwner {
+        Item item = items[_index]._item;
+        require(items[_index]._step == SupplyChainSteps.Paid, "item not paid");
+        items[_index]._step = SupplyChainSteps.Delivered;
+        emit SupplyChainStep(_index, uint(items[_index]._step), address(item)); 
     }
 }
